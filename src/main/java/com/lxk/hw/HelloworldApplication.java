0624e47a5b3a06dc3d9e05d99fc32f9813121aae
zx48822821 <752138572@qq.com>
2019-03-27 16:39:29 +0800

    Details:     1. Change to Service providers

--- src/main/java/com/lxk/hw/HelloworldApplication.java
+++ src/main/java/com/lxk/hw/HelloworldApplication.java
@@ -3,17 +3,22 @@ package com.lxk.hw;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+
 @SpringBootApplication
 @EnableCaching
+@EnableEurekaClient
+
 public class HelloworldApplication {
 
 	@RequestMapping("hello")
 	@ResponseBody
-	public String hello(){
-		return "hello world！";
+	public String hello(HttpServletRequest request){
+		return request.getRequestURL().toString()+"  hello world！";
 	}
 
 	public static void main(String[] args) {
@@ -21,3 +26,4 @@ public class HelloworldApplication {
 	}
 
 }
+
